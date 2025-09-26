0c6b2afc470a723495af9b8f54d5e0200a9e43d6
Gilbert Brandow <simon@0100.se>
2025-09-26 21:18:12 +0200

    Updated code and uploaded report

--- Lab 1/lab_1.r
+++ Lab 1/lab_1.r
@@ -12,7 +12,9 @@ data3 <- rnorm(1000, mean = 0, sd = sqrt(2))
 data4 <- rnorm(1000, mean = 0, sd = sqrt(3))
 
 par(mfrow = c(1,3))
+xlim <- c(-6, 6)
+ylim <- c(0, 250)
 
-hist(data2, main = "N(0,1)", xlab = "Value", border = "black", breaks = 20)
-hist(data3, main = "N(0,2)", xlab = "Value", border = "black", breaks = 20)
-hist(data4, main = "N(0,3)", xlab = "Value", border = "black", breaks = 20)
+hist(data2, xlim = xlim, ylim = ylim, main = "N(0,1)", xlab = "Value", border = "black", breaks = 20)
+hist(data3, xlim = xlim, ylim = ylim, main = "N(0,2)", xlab = "Value", border = "black", breaks = 20)
+hist(data4, xlim = xlim, ylim = ylim, main = "N(0,3)", xlab = "Value", border = "black", breaks = 20)
